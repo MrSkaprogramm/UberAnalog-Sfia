8d359f54b89e8adc15a98fc142e7beade8888c64
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-04 11:39:18 +0300

    Create DataSourceCarDAOTest.java

---
+++ src/test/java/by/epam/tr/dao/impl/DataSourceCarDAOTest.java
@@ -0,0 +1,13 @@
+import static org.mockito.Mockito.mock;
+import org.junit.Test;
+import by.epam.tr.datasource.DataSource;
+
+public class DataSourceCarDAOTest {
+
+  @Test
+  public void testGetRandomCarModel() {
+    DataSource dataSource = mock(DataSource.class);
+
+    // fail("Not yet implemented");
+  }
+}
