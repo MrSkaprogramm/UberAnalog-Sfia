3df7939a9e41f825b358491cb663f8e3b88b58f4
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-04 11:30:40 +0300

    Create DriversSortByStreet.java

---
+++ src/main/java/by/epam/tr/utils/DriversSortByStreet.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+import by.epam.tr.beans.Driver;
+
+/**
+ * Класс сортировки водителей в зависимости от улицы
+ */
+public class DriversSortByStreet implements Comparator<Driver> {
+  /**
+   * Сравнивает двух водителей по улице
+   * @return Возвращает результат сравнения
+   */
+  public int compare(Driver o1, Driver o2) {
+    return o1.getMap().getDriverLocation().compareTo(o2.getMap().getDriverLocation());
+  }
+}
