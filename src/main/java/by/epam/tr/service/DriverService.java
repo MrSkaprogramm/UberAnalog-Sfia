2f26890df3c4dff0675ddd60556ef85f9a837b88
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-04 11:27:58 +0300

    Create DriverService.java

---
+++ src/main/java/by/epam/tr/service/DriverService.java
@@ -0,0 +1,39 @@
+import by.epam.tr.beans.Driver;
+import by.epam.tr.beans.Rate;
+
+/**
+ * Интерфейс слоя Service для bean
+ * @see Driver
+ */
+public interface DriverService {
+  /**
+   * Возвращает boolean - значение правильности ввода данных водителя
+   */
+  public boolean validate(String name, String surname, String legalEntityName);
+
+  /**
+   * Возвращает ответ в виде boolean - зачения о регистрации нового водителя
+   */
+  public boolean register(String name, String surname, String legalEntityName);
+
+  /**
+   * Возвращает ответ в виде boolean - зачения о выборе автомобиля водителем
+   */
+  public boolean selectCar(String carModel, String carNumber);
+
+  /**
+   * Возвращает ответ в виде boolean - зачения
+   */
+  public boolean generateDrivers();
+
+  /**
+   * Выбирает первого в списке водителя рядом с пассажиром
+   */
+  public Driver chooseDriver();
+
+  /**
+   * Удаляет всех водителей в списке, не соответствующих выбранному пассажиром тарифу и записывает
+   * соответствующих водителей в список в базе данных
+   */
+  public boolean choiseDriverByRate(Rate rate);
+}
