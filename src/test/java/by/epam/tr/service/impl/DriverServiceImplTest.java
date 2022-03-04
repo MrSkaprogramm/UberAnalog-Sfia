981c7e7a83c8346d28bdab4fcef2ed39302050ae
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-04 11:40:17 +0300

    Create DriverServiceImplTest.java

---
+++ src/test/java/by/epam/tr/service/impl/DriverServiceImplTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import by.epam.tr.dao.MapDAO;
+
+public class DriverServiceImplTest {
+  @Mock
+  private MapDAO mapDAO;
+
+  @InjectMocks
+  private DriverServiceImpl driverServiceImpl;
+
+
+
+  // @SuppressWarnings("deprecation")
+  public DriverServiceImplTest() {
+    MockitoAnnotations.openMocks(this);
+    this.driverServiceImpl = new DriverServiceImpl();
+  }
+
+
+
+  @Test
+  public void testRegister() {
+    String name = "wjrngj";
+    String surname = "woinrg";
+    String legalEntityName = "oirnh";
+
+    given(mapDAO.getRandomLocation()).willReturn("Mazurova");
+    // assertTrue(driverServiceImpl.register(name, surname, legalEntityName));
+    boolean driverRegister = driverServiceImpl.register(name, surname, legalEntityName);
+
+    assertTrue(driverRegister);
+    // fail("Not yet implemented");
+  }
+}
