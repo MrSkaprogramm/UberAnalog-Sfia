cf5e6ec35bf7653cf173188e70071c3f462925d7
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-06 23:55:25 +0300

    Add files via upload

--- src/main/java/by/epam/tr/datasource/DataSource.java
+++ src/main/java/by/epam/tr/datasource/DataSource.java
@@ -1,32 +1,29 @@
 package by.epam.tr.datasource;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import by.epam.tr.beans.Driver;
-import by.epam.tr.beans.Order;
 import by.epam.tr.beans.Passenger;
 import by.epam.tr.beans.Rate;
 import by.epam.tr.utils.DriversSortByStreet;
 
 /**
- * Класс реализации базы данных коллекцией
+ * Database implementation class by collection
  */
 public class DataSource {
   /**
-   * @see DataSource#streetsLocationsFromCentre Улицы города и дистанция от центра
-   * @see DataSource#suitableCarsTaxis Машины, допустимые для поездок в такси
-   * @see DataSource#drivers Список водителей
-   * @see DataSource#passengers Список пассажиров
-   * @see DataSource#orders Список заказов
+   * @see DataSource#streetsLocationsFromCentre City streets and distance from the center
+   * @see DataSource#suitableCarsTaxis Cars allowed for Taxi rides
+   * @see DataSource#drivers List of drivers
+   * @see DataSource#passengers Passenger List
+   * @see DataSource#orders List of orders
    */
   private Map<String, Double> streetsLocationsFromCentre = new HashMap<String, Double>();
   private Map<String, Rate> suitableCarsTaxis = new HashMap<String, Rate>();
   private Set<Driver> drivers = new TreeSet<Driver>(new DriversSortByStreet());
   private Set<Passenger> passengers = new TreeSet<Passenger>();
-  private List<Order> orders = new ArrayList<Order>();
 	
 	public DataSource() {}
 
@@ -60,13 +57,5 @@ public class DataSource {
 
     public void setPassengers(Set<Passenger> passengers) {
       this.passengers = passengers;
-    }
-
-    public List<Order> getOrders() {
-      return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-      this.orders = orders;
     }
 }
--- src/main/java/by/epam/tr/datasource/Initialization.java
+++ src/main/java/by/epam/tr/datasource/Initialization.java
@@ -2,59 +2,60 @@ package by.epam.tr.datasource;
 
 import by.epam.tr.beans.Rate;
 import by.epam.tr.dao.CarDAO;
+import by.epam.tr.dao.DAOException;
 import by.epam.tr.dao.DAOProvider;
 import by.epam.tr.dao.MapDAO;
 
 /**
- * Класс инициализации базы данных в начале программы
- * @see DataSource
+ * Database initialization class at the beginning of the program * @see DataSource
  */
 public class Initialization {
   /**
-   * Инициализация объектов слоя DAO с помощью DAOProvider
+   * Initialization of DAO layer objects using DAOProvider
    */
   private final DAOProvider provider = DAOProvider.getDaoProvider();
   private CarDAO carDAO = provider.getCarDAO();
   private MapDAO mapDAO = provider.getMapDAO();
 
   /**
-   * Метод загрузки списка улиц города в базу данных
+   * Method of loading a list of city streets into a database
    */
-	public void initStreetsDatabase() {
-      mapDAO.getStreets().put("Aivazovsky", 6.5);
-      mapDAO.getStreets().put("Angarsk", 11.8);
-      mapDAO.getStreets().put("Ales Dudara", 8.2);
-      mapDAO.getStreets().put("Arkady Smolich", 8.4);
-      mapDAO.getStreets().put("Avtodorovskaya", 4.9);
-      mapDAO.getStreets().put("Alibegova", 6.7);
-      mapDAO.getStreets().put("Avakyan", 3.9);
-      mapDAO.getStreets().put("Independence Ave", 6.4);
-      mapDAO.getStreets().put("Avangardnaya", 6.3);
-      mapDAO.getStreets().put("Vaupshasova", 7.8);
-      mapDAO.getStreets().put("Academician Vysotsky", 7.8);
-      mapDAO.getStreets().put("Kalinina", 4.8);
-      mapDAO.getStreets().put("Melezha", 3.7);
-      mapDAO.getStreets().put("Yakub Kolas", 5.8);
-      mapDAO.getStreets().put("Mazurova", 10.9);
-      mapDAO.getStreets().put("Briquette", 10.7);
-      mapDAO.getStreets().put("International", 0.4);
-      mapDAO.getStreets().put("Ratomskaya", 9.0);
-	}
+  public void initStreetsDatabase() {
+    try {
+      mapDAO.addStreet("Aivazovsky" + " " + 6.5);
+      mapDAO.addStreet("Avtodorovskaya" + " " + 4.9);
+      mapDAO.addStreet("Alibegova" + " " + 6.7);
+      mapDAO.addStreet("Avakyan" + " " + "3.9");
+      mapDAO.addStreet("Independence" + " " + "6.4");
+      mapDAO.addStreet("Avangardnaya" + " " + "6.3");
+      mapDAO.addStreet("Vaupshasova" + " " + "7.8");
+      mapDAO.addStreet("Vysotsky" + " " + "7.8");
+      mapDAO.addStreet("Kalinina" + " " + "4.8");
+      mapDAO.addStreet("Melezha" + " " + "3.7");
+      mapDAO.addStreet("Kolas" + " " + "5.8");
+      mapDAO.addStreet("Mazurova" + " " + "10.9");
+      mapDAO.addStreet("Briquette" + " " + "10.7");
+      mapDAO.addStreet("International" + " " + "0.4");
+      mapDAO.addStreet("Ratomskaya" + " " + "9.0");
+    } catch (DAOException e) {
+      e.printStackTrace();
+    }
+  }
 
-    /**
-     * Метод загрузки списка автомобилей в базу данных
-     */
-    public void initCarsDatabase() {
-      carDAO.getCarsSuitableTaxis().put("VW POLO", Rate.MINIMAL);
-      carDAO.getCarsSuitableTaxis().put("Renault Logan", Rate.MINIMAL);
-      carDAO.getCarsSuitableTaxis().put("KIA RIO", Rate.MINIMAL);
-      carDAO.getCarsSuitableTaxis().put("CITROEN C3", Rate.MINIMAL);
-      carDAO.getCarsSuitableTaxis().put("Honda Accord", Rate.MIDDLE);
-      carDAO.getCarsSuitableTaxis().put("VW PASSAT", Rate.MIDDLE);
-      carDAO.getCarsSuitableTaxis().put("TOYOTA CAMRY", Rate.MIDDLE);
-      carDAO.getCarsSuitableTaxis().put("JAGUAR XF", Rate.MIDDLE_PLUS);
-      carDAO.getCarsSuitableTaxis().put("AUDI A5", Rate.MIDDLE_PLUS);
-      carDAO.getCarsSuitableTaxis().put("MERCEDES S600", Rate.LUX);
-      carDAO.getCarsSuitableTaxis().put("TESLA MODELS", Rate.LUX);
-	}
+  /**
+   * Method of loading the list of cars into the database
+   */
+  public void initCarsDatabase() {
+    carDAO.getCarsSuitableTaxis().put("VW POLO", Rate.MINIMAL);
+    carDAO.getCarsSuitableTaxis().put("RENAULT LOGAN", Rate.MINIMAL);
+    carDAO.getCarsSuitableTaxis().put("KIA RIO", Rate.MINIMAL);
+    carDAO.getCarsSuitableTaxis().put("CITROEN C3", Rate.MINIMAL);
+    carDAO.getCarsSuitableTaxis().put("HONDA ACCORD", Rate.MIDDLE);
+    carDAO.getCarsSuitableTaxis().put("VW PASSAT", Rate.MIDDLE);
+    carDAO.getCarsSuitableTaxis().put("TOYOTA CAMRY", Rate.MIDDLE);
+    carDAO.getCarsSuitableTaxis().put("JAGUAR XF", Rate.MIDDLE_PLUS);
+    carDAO.getCarsSuitableTaxis().put("AUDI A5", Rate.MIDDLE_PLUS);
+    carDAO.getCarsSuitableTaxis().put("MERCEDES S600", Rate.LUX);
+    carDAO.getCarsSuitableTaxis().put("TESLA MODELS", Rate.LUX);
+  }
 }
